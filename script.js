04f1d8d5f83f149fe7c3cf1a506d40a9e6505f20
valdezjulian <julian.valdez531@gmail.com>
2024-07-01 20:57:53 -0400

    primeiro-commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", () => {
+    navbar.classList.toggle("show-menu");
+});
+
+document.querySelector('a[href="#home"]').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
